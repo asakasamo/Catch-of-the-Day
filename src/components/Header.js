34e7cd770459f7c8ca190b63fd0eb1a664af2b73
asakasamo <a.sakasamo@gmail.com>
2018-07-01 15:13:51 -0500

    Added PropTypes to all components (to validate data types)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Header extends React.Component {
    render() {
       return (
          <header className="top">
             <h1>
-               Catch 
+               Catch
                <span className="ofThe">
                   <span className="of">of</span>
                   <span className="the">the</span>
                </span>
-               Day   
+               Day
             </h1>
             <h3 className="tagline">
                <span>{this.props.tagline}</span>
@@ -19,6 +20,5 @@ class Header extends React.Component {
       );
    }
 }
-
 
 export default Header;
