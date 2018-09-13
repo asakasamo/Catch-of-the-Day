7d91d03ef4bb0d198269364f99e9b824fb7e146f
asakasamo <a.sakasamo@gmail.com>
2018-09-13 03:24:43 -0500

    Change router settings (for proper routing on gh-pages)

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from './App'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./App";
 import StorePicker from "./StorePicker";
 import NotFound from "./NotFound";
 
 const Router = () => (
-   <BrowserRouter>
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-         <Route exact path="/" component={ StorePicker }/>
-         <Route path="/store/:storeId" component={ App }/>
-         <Route component={ NotFound }/>
+         <Route exact path="/" component={StorePicker} />
+         <Route path="/store/:storeId" component={App} />
+         <Route component={NotFound} />
       </Switch>
    </BrowserRouter>
-)
+);
 
 export default Router;
-
