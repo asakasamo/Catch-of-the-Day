e49e0c9a1c7f89dd8a50d81f1d79b21f0f13ef21
asakasamo <a.sakasamo@gmail.com>
2018-09-16 21:59:29 -0500

    Change router to HashRouter for gh-pages compatibility

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import StorePicker from "./StorePicker";
 import NotFound from "./NotFound";
 
 const Router = () => (
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
+   <HashRouter>
       <Switch>
          <Route exact path="/" component={StorePicker} />
          <Route path="/store/:storeId" component={App} />
          <Route component={NotFound} />
       </Switch>
-   </BrowserRouter>
+   </HashRouter>
 );
 
 export default Router;
